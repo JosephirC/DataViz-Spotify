24e5fcfd29a49cb7376e6ece2142561a5082eb82
Yvars Arthur <arthur.yvars@etu.univ-lyon1.fr>
2026-01-12 20:48:01 +0100

    update progres et fix pages 2 a cause du nom de data changed

--- src/scripts/page2.js
+++ src/scripts/page2.js
@@ -74,7 +74,7 @@ const tooltip = d3.select("body")
   .style("pointer-events", "none")
   .style("opacity", 0);
 
-d3.csv("../data/top_50_from_2023_to_2025.csv", d => {
+d3.csv("../data/top_50_71Countries_from_2023_to_2025.csv", d => {
   const date = new Date(d.album_release_date);
 
   if (isNaN(date))return null;
